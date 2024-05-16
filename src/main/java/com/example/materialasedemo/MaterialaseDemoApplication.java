a10c776e4eeef6f33e01a204ee4c2548fc5ebf3e
Alexander <alexl1967@mail.ru>
2024-05-16 14:00:12 +0300

    Initial commit

---
+++ src/main/java/com/example/materialasedemo/MaterialaseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.materialasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialaseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaterialaseDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/materialasedemo/MaterialaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.materialasedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaterialaseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
