6022c946fdcb8fb9def2692dce61adbe8641ccef
Alexander <alexl1967@mail.ru>
2024-05-16 14:00:59 +0300

    phase0

--- src/main/java/com/example/materialasedemo/MaterialaseDemoApplication.java
+++ src/main/java/com/example/materialasedemo/MaterialaseDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MaterialaseDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MaterialaseDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MaterialaseDemoApplication.class, args);
+    }
 
 }
